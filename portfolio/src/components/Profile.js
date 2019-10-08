ae4f71e252dc3e79bdad68e4f8709117a5288797
AidenBow <AidenBow@users.noreply.github.com>
2019-10-08 14:46:57 -0700

    created profile

---
+++ portfolio/src/components/Profile.js
@@ -0,0 +1,18 @@
+import React from "react"
+
+const Profile = () => {
+    return (
+        <div className="profileContainer">
+            <section className="profileText">
+                <h1>Aiden Bowcott</h1>
+                <p>Web Developer</p>
+            </section>
+            <section>
+                <img alt="Aiden's face!" className="profileIMG" src="https://avatars0.githubusercontent.com/u/49966384?s=400&u=6073710f9d6567a0de54a2f015efc1219c096253&v=4"></img>
+            </section>
+
+        </div>
+    )
+}
+
+export default Profile
