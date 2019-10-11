9640a851c523c148483ed3167a03c086dde65852
AidenBow <AidenBow@users.noreply.github.com>
2019-10-10 18:24:15 -0700

    added pokemon imgs

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import './App.css';
 
 import Profile from "./components/Profile"
+import Projects from "./components/Projects"
 
 function App() {
   return (
     <div className="App">
       <Profile />
+      <Projects />
       <img alt="under construction" style={{margin: "60px 0px", width: "100%", maxWidth: 300}} src="https://media.giphy.com/media/S5JSwmQYHOGMo/200.gif"></img>
     </div>
   );
---
+++ portfolio/src/components/Projects.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+const Projects = () => {
+    const pokeIMG = require("../img/pokemonAPI.png")
+    return (
+        <div>
+            <h3>Pokemon API</h3>
+            <img src={pokeIMG} alt="screenshot of pokemon website"></img>
+        </div>
+    )
+}
+
+export default Projects
