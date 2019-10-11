f597a8ad4be19a550e1ac91ede579aa7b0ed6c85
AidenBow <AidenBow@users.noreply.github.com>
2019-10-10 18:36:51 -0700

    added pokemon api project card

--- portfolio/src/components/Projects.js
+++ portfolio/src/components/Projects.js
@@ -1,11 +1,14 @@
 import React from "react"
 
 const Projects = () => {
-    const pokeIMG = require("../img/pokemonAPI.png")
     return (
         <div>
+            <section className="projectCard" >
             <h3>Pokemon API</h3>
-            <img src={pokeIMG} alt="screenshot of pokemon website"></img>
+            <a href="https://thunker.bow.now.sh/">
+                <img className="screenshot"  src={require('../img/pokemonAPI.png')} alt="screenshot of pokemon website"></img>
+            </a>
+            </section>
         </div>
     )
 }
